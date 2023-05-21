d64f06fe25902600b84dcdba77c26ba5bc0d5f7f
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-05-21 00:34:42 -0400

    update

--- crud/pages/home.js
+++ crud/pages/home.js
@@ -1,9 +1,31 @@
 
+import { useEffect } from "react"
+import {app} from "../firebaseConfig"
+import { useRouter } from "next/router"
 
 export default function Home() {
+  let router = useRouter()
+  useEffect(() => {
+    let token = sessionStorage.getItem('Token')
+    if (token) {
+      getData()
+    }
+    if (!token) {
+      router.push('/register')
+    }
+  }, [])
+
   return (
    <div>
     <h2>Home Screen</h2>
+    <input
+      placeholder="Name"
+    />
+
+    <input
+    placeholder="Age"
+    />
+    <button>Add</button>
    </div>
   )
 }
